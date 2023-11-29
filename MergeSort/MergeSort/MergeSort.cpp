75423682058b5b0a56c28c1a5c570108f42bcb23
Tord-Elias <tordeliashoumb46@gmail.com>
2023-11-29 20:47:50 +0100

    Coded MergeSort

--- MergeSort/MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort/MergeSort.cpp
@@ -3,14 +3,44 @@
 
 #include <iostream>
 #include <list>
+#include <chrono>
+
+
 using namespace std; 
+
+
+void Merge(std::list<int>& l, std::list<int>& left, std::list<int>& right) 
+{
+   
+    auto i = left.begin(), j = right.begin();
+
+    while (i != left.end() && j != right.end()) {
+        if (*i <= *j) {
+            l.push_back(*i);
+            ++i;
+        }
+        else {
+            l.push_back(*j);
+            ++j;
+        }
+    }
+
+    while (i != left.end()) {
+        l.push_back(*i);
+        ++i;
+    }
+
+    while (j != right.end()) {
+        l.push_back(*j);
+        ++j;
+    }
+}
 
 void MergeSort(list<int> &l)
 {
     int size = l.size(); 
     list<int> left; 
     list<int> right; 
-    auto it = l.begin(); 
     while (l.size() != size / 2 && size != 1)
     {
         left.push_back(l.front()); 
@@ -20,44 +50,39 @@ void MergeSort(list<int> &l)
     {
         right.swap(l); 
         l.clear(); 
+        MergeSort(left);
+        MergeSort(right);
     }
-    MergeSort(left);
-    MergeSort(right);
-    if (right.front() < left.front())
-    {
-        if (right.back() < left.back())
-        {
-            l.merge(right);
-            l.merge(left);
-        }
-        else
-        {
-            //merge in middle 
-        }
-        
-    }
-    if (right.front() > left.front())
-    {
-        if (right.back() > left.back())
-        {
-            l.merge(left);
-            l.merge(right);
-        }
-        else
-        {
-            //merge in middle 
-            // kan hende jeg må bruke splice eller hva det heter a
-        }
+    Merge(l, left, right); 
+}
 
+void PrintList(list<int> l)
+{
+    for (auto first : l)
+    {
+        cout << first << " "; 
     }
-
 }
 int main()
 {
     list<int> mainList; 
+    int listSize = 10000;
+    list<int> mList;
+    srand(time(NULL));
+    for (int i = 0; i < listSize; i++)
+    {
+        mainList.push_back(rand() % 10000 + 1);
+    }
+   
+    auto start = std::chrono::high_resolution_clock::now();
+    MergeSort(mainList); 
+    auto end = std::chrono::high_resolution_clock::now();
+    cout << "sorted list\n"; 
+    PrintList(mainList); 
+    cout << "\n"; 
 
+    std::cout << "Time taken" << std::chrono::duration_cast<std::chrono::milliseconds>((end)-(start)).count() << " ms" << std::endl;
 
-    std::cout << "Hello World!\n";
 }
 
 
